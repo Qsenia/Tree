6d716041967b2dad3a2a39ef1ff0f0da1e0bc176
Qsenia <oksana.kholnova@gmail.com>
2023-03-17 22:40:30 +0300

    Correcting in PersonNameNobilityComparator

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,16 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
-        people.add(new Person("Анна", "Хольнова", 4));
-        people.add(new Person("Лидия", "Хольнова", 7));
-        people.add(new Person("Оксана", "Глушенкова", 10));
-        people.add(new Person("Ольга", "Глушенкова", 15));
-        people.add(new Person("Федор", "Кац", 3));
+        people.add(new Person("Анна", "Хольнова-Кумач", 4));
+        people.add(new Person("Лидия", "Хольнова-Кумач", 7));
+        people.add(new Person("Оксана", "Ван Дейк", 10));
+        people.add(new Person("Ольга", "Ван Дейк", 15));
+        people.add(new Person("Федор", "Кац", 36));
         people.add(new Person("Максим", "Ким", 2));
+        people.add(new Person("Габриэль", "Аломар-и-Вильялонга",11));
 
         System.out.println(people);
-        Collections.sort(people, new PersonNameNobilityComparator(9));
+        Collections.sort(people, new PersonNameNobilityComparator(3));
         System.out.println(people);
 
     }
--- src/PersonNameNobilityComparator.java
+++ src/PersonNameNobilityComparator.java
@@ -1,10 +1,12 @@
 import java.util.Comparator;
 
 public class PersonNameNobilityComparator implements Comparator<Person> {
-    public int maxLetters;
+    public int maxWords;
+    public String[] newSurname1;
+    public String[] newSurname2;
 
-    public PersonNameNobilityComparator(int maxLetters) {
-        this.maxLetters = maxLetters;
+    public PersonNameNobilityComparator(int maxWords) {
+        this.maxWords = maxWords;
     }
     //Добавьте в ваш компаратор конструктор,
 // в котором указывается максимальное количество слов в фамилии,
@@ -13,19 +15,24 @@ public class PersonNameNobilityComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        if (o1.getSurname().length() == o2.getSurname().length()) {
+
+        newSurname1 = o1.getSurname().split("[ -]");
+        newSurname2 = o2.getSurname().split("[ -]");
+
+        if (newSurname2.length == newSurname1.length) {
             return Integer.compare(o2.getAge(), o1.getAge());
             //Если количества слов в фамилии одинаковы, то знатен больше тот, кто старше
         }
-        if ((o1.getSurname().length() >= maxLetters) && (o2.getSurname().length() >= maxLetters)) {
+        if ((newSurname2.length >= maxWords) && (newSurname1.length >= maxWords)) {
             // если обе фамилии состоят из количества слов не меньше чем число,
             // указанное в конструкторе, то обе фамилии равнозначны и по ним нельзя определить знатность
             // (что нужно продолжать сравнение уже по возрасту)
             return Integer.compare(o2.getAge(), o1.getAge());
-        } else {
-            return Integer.compare(o2.getSurname().length(), o1.getSurname().length());
         }
-
+        if ((newSurname1.length < maxWords) && (newSurname2.length < maxWords)) {
+            return Integer.compare(newSurname2.length, newSurname1.length);
+        }
+        return Integer.compare(newSurname2.length, newSurname1.length);
 
     }
 }
